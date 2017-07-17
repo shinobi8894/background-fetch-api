1113de2602c5b7fa658d856a05d348060506e735
System <none>
2017-07-17 11:01:32 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  /*if (request.protocol == 'http') {
+  if (request.get('X-Forwarded-Proto') == 'http') {
     response.redirect(301, 'https://f1-start.glitch.me');
     return;
-  }*/
+  }
   response.set('Cache-Control', 'no-cache');
   response.sendFile(__dirname + '/views/index.html');
 });
