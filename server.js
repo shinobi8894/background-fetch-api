af7a5592ee5488df4b861d7af07380cd1e652675
System <none>
2017-07-26 12:19:25 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-app.get("/sw.js", function (request, response) {
-  response.set('Cache-Control', 'no-cache');
-  response.sendFile(__dirname + '/public/sw.js');
-});
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  if (!request.get('X-Forwarded-Proto').includes('https')) {
-    response.redirect(301, 'https://f1-start.glitch.me');
+app.use((req, res, next) => {
+  // Forward to HTTPS
+  if (!req.get('X-Forwarded-Proto').includes('https')) {
+    // TODO: fix this
+    //res.redirect(301, 'https://f1-start.glitch.me');
     return;
   }
-  response.set('Cache-Control', 'no-cache');
-  response.sendFile(__dirname + '/views/index.html');
+  res.set('Cache-Control', 'no-cache');
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
