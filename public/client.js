da326a32c268a5b4c891a6ed305135bd442e95fc
Glitch (basic-express) <none>
2018-10-04 11:59:59 +0000

    ⛩🌎 Checkpoint ./public/client.js:328454/562

--- public/client.js
+++ public/client.js
@@ -1,15 +1,27 @@
 import { html, render as litRender } from '/lit/lit-html.js';
 
+const app = document.querySelector('.app');
+
+const template = (items) => html`
+  ${items.map(item => html`
+    <h2>${item.title}</h2>
+    <p>${item.subtitle}</p>
+    <img src=${item.image} width="100" height="100">
+  `)}
+`;
+
 function render(items) {
-  
+  litRender(template(items), app);
 }
 
 async function fetchFeed() {
   const response = await fetch('/feed');
   const dom = new DOMParser().parseFromString(await response.text(), 'text/xml');
-  const items = [...dom.querySelectorAll('item')].map(domItem => ({
-    title: domItem.querySelector('title').textContent
+  return [...dom.querySelectorAll('item')].map(domItem => ({
+    title: domItem.querySelector('title').textContent,
+    subtitle: domItem.querySelector('subtitle').textContent,
+    image: new URL(domItem.querySelector('image').getAttribute('href'), 'https://developers.google.com/').href
   }));
 }
 
-fetchFeed();
+fetchFeed().then(items => render(items));
