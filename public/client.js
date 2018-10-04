21e5ca5bc373b621f389744acb3e957e87388fc1
Glitch (basic-express) <none>
2018-10-04 11:49:57 +0000

    🙍🏍 Checkpoint ./public/client.js:328454/365

--- public/client.js
+++ public/client.js
@@ -1,6 +1,15 @@
-import { html } from '/lit/lit-html.js';
+import { html, render as litRender } from '/lit/lit-html.js';
 
-function fetchFeed() {
-  fetch('/feed');
+function render(items) {
+  
 }
 
+async function fetchFeed() {
+  const response = await fetch('/feed');
+  const dom = new DOMParser().parseFromString(await response.text(), 'text/xml');
+  const items = [...dom.querySelectorAll('item')].map(domItem => ({
+    title: domItem.querySelector('title').textContent
+  }));
+}
+
+fetchFeed();
