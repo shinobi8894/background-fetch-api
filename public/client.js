bf5a1269bb989f88c423ed58a98b5cbfafc08d0d
Glitch (basic-express) <none>
2018-10-06 14:04:29 +0000

    🎈👙 Checkpoint ./public/client.js:328454/47 ./public/index.html:328454/247

--- public/client.js
+++ public/client.js
@@ -221,6 +221,7 @@ async function fallbackFetch(item) {
     if (done) break;
     downloaded += value.length;
     chunks.push(value);
+    console.log(downloaded / item.size);
     updateItem(item.id, { progress: downloaded / item.size });
   }
   
